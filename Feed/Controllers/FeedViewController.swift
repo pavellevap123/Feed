c8ef418b498f95119aab34ee94060a929af57728
Pavel Poddubotskiy <pavel.poddubotskiy@pm.by>
2022-09-01 16:49:48 +0300

    feat: Header view

--- Feed/Controllers/FeedViewController.swift
+++ Feed/Controllers/FeedViewController.swift
@@ -9,28 +9,46 @@ import UIKit
 
 final class FeedViewController: UIViewController {
     
-    private lazy var label: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "Hello challenge!"
-        return label
-    }()
+    private let headerView = HeaderView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        style()
         layout()
-        self.title = "1.357 P"
+    }
+    
+    func style() {
+        view.backgroundColor = UIColor(named: "BackgroundColor")
         
-        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.blue]
+        //NavigationBar
+        self.title = "1.357 P"
+        navigationController!.navigationBar.isTranslucent = false
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.systemBlue]
+        
+        //HeaderView
+        view.addSubview(headerView)
+        headerView.addShadowToView(shadow_color: UIColor.gray, offset: CGSize(width: 0, height: 3), shadow_radius: 5.0, shadow_opacity: 0.5, corner_radius: 0.0)
     }
     
     private func layout() {
-        view.addSubview(label)
-        
+        //Header
         NSLayoutConstraint.activate([
-            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            headerView.heightAnchor.constraint(equalToConstant: 50),
+            
+            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
     }
 }
 
+extension UIView {
+    
+    public func addShadowToView(shadow_color: UIColor,offset: CGSize,shadow_radius: CGFloat,shadow_opacity: Float,corner_radius: CGFloat) {
+        self.layer.shadowColor = shadow_color.cgColor
+        self.layer.shadowOpacity = shadow_opacity
+        self.layer.shadowOffset = offset
+        self.layer.shadowRadius = shadow_radius
+        self.layer.cornerRadius = corner_radius
+    }
+}
--- Feed/Views/HeaderView.swift
+++ Feed/Views/HeaderView.swift
@@ -5,4 +5,80 @@
 //  Created by Pavel Poddubotskiy on 1.09.22.
 //
 
-import Foundation
+import UIKit
+
+final class HeaderView: UIView {
+    
+    private lazy var separator: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .separator
+        return view
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.spacing = 5
+        return stackView
+    }()
+    
+    private lazy var image: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "GoLabel")
+        return imageView
+    }()
+    
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .systemBlue
+        label.text = "Partner erkunden"
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        style()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: 200, height: 200)
+    }
+}
+
+extension HeaderView {
+    
+    func style() {
+        backgroundColor = .white
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        addSubview(separator)
+        addSubview(stackView)
+        
+        stackView.addArrangedSubview(image)
+        stackView.addArrangedSubview(label)
+    }
+    
+    func layout() {
+        
+        NSLayoutConstraint.activate([
+            //Separator
+            separator.heightAnchor.constraint(equalToConstant: 1),
+            
+            separator.topAnchor.constraint(equalTo: topAnchor),
+            separator.leadingAnchor.constraint(equalTo: leadingAnchor),
+            separator.trailingAnchor.constraint(equalTo: trailingAnchor),
+            
+            //StackView
+            stackView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+}
