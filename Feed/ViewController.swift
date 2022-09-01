7ffd5adb9abff2c81165fb1f2b870d81d24cf9d1
Pavel Poddubotskiy <pavel.poddubotskiy@pm.by>
2022-09-01 15:11:02 +0300

    Initial Commit

---
+++ Feed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Feed
+//
+//  Created by Pavel Poddubotskiy on 1.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
